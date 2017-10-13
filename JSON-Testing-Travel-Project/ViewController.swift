24102660cf0f9f59a5dc598b4726cfa0a9f8bc36
matthewharrilal <matthewharrilal@gmail.com>
2017-10-12 18:13:41 -0700

    Simply grabbing the JSON object and seeing if I can get back anything in return

--- JSON-Testing-Travel-Project/ViewController.swift
+++ JSON-Testing-Travel-Project/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let instance = Network()
+        instance.networking()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +24,61 @@ class ViewController: UIViewController {
 
 
 }
+
+//So essentially what we are going to be doing in this file is just seeing if we can grab a json object back from the data
+
+class Network {
+    func networking() {
+//        First we have to declare the session that we will be performing the data tasks on
+        let session = URLSession.shared
+//        Then after we do this what we essentially have to do is that we make the get request
+        var url = URL(string: "http://127.0.0.1:5000/trips")
+        let urlParams = ["email": "stevenuniverse@gmail.com"]
+        url = url?.appendingQueryParameters(urlParams)
+        var getRequest = URLRequest(url: url!)
+        getRequest.httpMethod = "GET"
+        session.dataTask(with: getRequest) { (data, response, error) in
+            if let data = data {
+                let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
+           
+                print(json)
+            }
+        }.resume()
+    }
+}
+
+extension URL {
+    func appendingQueryParameters(_ parametersDictionary : Dictionary<String, String>) -> URL {
+        let URLString : String = String(format: "%@?%@", self.absoluteString, parametersDictionary.queryParameters)
+        return URL(string: URLString)!
+    }
+    // This is formatting the query parameters with an ascii table reference therefore we can be returned a json file
+}
+
+protocol URLQueryParameterStringConvertible {
+    var queryParameters: String {get}
+}
+
+
+extension Dictionary : URLQueryParameterStringConvertible {
+    /**
+     This computed property returns a query parameters string from the given NSDictionary. For
+     example, if the input is @{@"day":@"Tuesday", @"month":@"January"}, the output
+     string will be @"day=Tuesday&month=January".
+     @return The computed parameters string.
+     */
+    var queryParameters: String {
+        var parts: [String] = []
+        for (key, value) in self {
+            let part = String(format: "%@=%@",
+                              String(describing: key).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!,
+                              String(describing: value).addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+            parts.append(part as String)
+        }
+        return parts.joined(separator: "&")
+    }
+    
+}
+
+
 
